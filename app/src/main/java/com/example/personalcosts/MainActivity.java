c97596384dc84a5d799006105de2490987317485
JohanaZamora <147088515+JohanaZamora@users.noreply.github.com>
2023-10-10 15:04:49 -0600

    Borrado todo lo actualizado

--- app/src/main/java/com/example/personalcosts/MainActivity.java
+++ app/src/main/java/com/example/personalcosts/MainActivity.java
@@ -27,8 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     MoneyDB MDB;
     ArrayList<String> idCategoria, NombreCategoria, ContenidoCategoria;
-    //Listas para guardar los datos de las categorias
-    CostosAdapter1 costosAdapter1; //adaptador para el recyclerView
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,53 +53,5 @@ public class MainActivity extends AppCompatActivity {
         idCategoria = new ArrayList<>();
         NombreCategoria = new ArrayList<>();
         ContenidoCategoria = new ArrayList<>();
-
-        //inicializa el adaptador y se configura el RecyclerView
-        costosAdapter1 = new CostosAdapter1(MainActivity.this, idCategoria, NombreCategoria, ContenidoCategoria);
-        recyclerView.setAdapter(costosAdapter1);
-        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-
-        //Se cargan los datos dentro de las listas y se actuliza el recyclerview
-        NuevaData();
-
-        //Se configura el boton para que permita al usuario cerrar sesion
-        iconosalir.setOnClickListener((v) -> {
-            new AlertDialog.Builder(MainActivity.this).setTitle("Cerrar sesión").
-                    setMessage("¿Esta seguro de cerrar sesión?").setPositiveButton(android.R.string.yes,
-                            (dialog, which) ->
-                            {
-                                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                            }).setNegativeButton(android.R.string.cancel, null).setIcon(R.drawable.warning).show();
-        });
-    }
-
-    void NuevaData(){
-        // Limpia las listas antes de agregar los datos
-        idCategoria.clear();
-        NombreCategoria.clear();
-        ContenidoCategoria.clear();
-
-        //Obtiene los datos de la bd
-        Cursor cursor = MDB.Data();
-
-        if(cursor.getCount() == 0){
-            Toast.makeText(this, "No hay datos", Toast.LENGTH_SHORT).show();
-        }else{
-            while (cursor.moveToNext()){
-                idCategoria.add(cursor.getString(0));
-                NombreCategoria.add(cursor.getString(1));
-                ContenidoCategoria.add(cursor.getString(2));
-            }
-            costosAdapter1.notifyDataSetChanged();
-        }
-    }
-    @Override
-    public void onBackPressed() {
-        finishAffinity();
-    }
-
-    protected void onResume() {
-        super.onResume();
-        NuevaData(); //Actualiza los datos cuando se reanuda la actividad
     }
 }
